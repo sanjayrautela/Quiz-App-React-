16d37e93ab2ae8025a65ad135270c07dbfbfe6ff
sanjayrautela <haribolsanjay2@gmail.com>
2024-04-15 01:04:09 -0700

    Updates

---
+++ question.js
@@ -0,0 +1,5 @@
+[
+    {"id": 1, "question": "What is 2 + 2?", "options": ["2", "3", "4", "5"], "answer": "4"},
+    {"id": 2, "question": "Capital of France?", "options": ["Paris", "London", "Berlin", "Madrid"], "answer": "Paris"},
+    
+]
